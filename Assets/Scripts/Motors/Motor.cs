ef377a0f7bbef5b971f3731f095c24065997f81d
RobinDduek <robin.dudek@ynov.com>
2018-12-21 10:13:17 +0100

    clean code

--- Assets/Scripts/Motors/Motor.cs
+++ Assets/Scripts/Motors/Motor.cs
@@ -24,14 +24,6 @@ public class Motor : MotorBase
     {
         if (hasCamera)
         {
-            if ( rotation.x > 0)
-            {
-                Debug.Log("Up");
-            } else
-            {
-                Debug.Log("Down");
-            }
-            Debug.Log(cam.transform.parent.localRotation.x);
             if(cam.transform.parent.localRotation.x > -0.3)
             {
                 if (cam.transform.parent.localRotation.x < 0.3)
@@ -46,16 +38,6 @@ public class Motor : MotorBase
             {
                 cam.transform.parent.Rotate(new Vector3(-rotation.x, 0, 0));
             }
-
-
-            /*if (cam.transform.parent.rotation..x - rotation.x > 30)
-            {
-                rotation.x = 30 - cam.transform.parent.rotation.eulerAngles.x;
-            } else if (cam.transform.parent.rotation.localEulerAngles.x - rotation.x < -30)
-            {
-                rotation.x = -30 - cam.transform.parent.rotation.eulerAngles.x;
-            }*/
-            //rotation.x = Mathf.Clamp(rotation.x, -30f, 30f);
         }
         entity.transform.Rotate(new Vector3(0, rotation.y, 0));
     }
