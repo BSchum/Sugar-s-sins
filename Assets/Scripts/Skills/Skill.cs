2c541eef4895ef950a31d0b57a9c9e5945f68a71
coolofdead <thomas.giovannoni@hotmail.fr>
2019-02-26 16:34:03 +0100

    Fusion des sorts fini

--- Assets/Scripts/Skills/Skill.cs
+++ Assets/Scripts/Skills/Skill.cs
@@ -8,8 +8,6 @@ public abstract class Skill : NetworkBehaviour {
     public float cost;
     public float threat;
 
-    public bool isMeltable;
-
     [HideInInspector]
     public bool canCast = true;
     [SerializeField]
