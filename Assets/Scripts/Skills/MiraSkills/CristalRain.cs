a8f0df721bd61ba388ac2a305cf776f7a6e89f6d
brice schumacher <brice.schumacher@ynov.com>
2019-04-06 18:57:53 +0200

    Spells icons + Feu vert mage + Title screen

--- Assets/Scripts/Skills/MiraSkills/CristalRain.cs
+++ Assets/Scripts/Skills/MiraSkills/CristalRain.cs
@@ -16,7 +16,6 @@ public class CristalRain : NetworkBehaviour {
         for (int i = 0; i < 10; i++)
         {
             Collider[] colls = Physics.OverlapSphere(this.transform.position, 8);
-
             IEnumerable<Collider> playerColls = colls.Where(c => c.tag == Constants.PLAYER_TAG);
 
             foreach (Collider col in playerColls)
@@ -27,5 +26,9 @@ public class CristalRain : NetworkBehaviour {
         }
 
         Destroy(this.gameObject);
+    }
+    private void OnDrawGizmos()
+    {
+        Gizmos.DrawWireSphere(this.transform.position, 8);
     }
 }
--- Assets/Scripts/UI/ButtonEvents.cs
+++ Assets/Scripts/UI/ButtonEvents.cs
@@ -4,27 +4,8 @@ using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class ButtonEvents : MonoBehaviour {
-
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
-
-    public void GoToChooseCharacter()
-    {
-        SceneManager.LoadScene("ChooseCharacter");
-    }
-
     public void GoToMainLevel()
     {
-        if(CharaterManager.choosedCharacter != null)
-        {
-            SceneManager.LoadScene("MainLevel");
-        }
+        SceneManager.LoadScene("MainLevel");    
     }
 }
